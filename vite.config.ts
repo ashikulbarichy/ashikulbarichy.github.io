f441dd6f41c64f2219a571dc3d9e148cf7aaf827
ashikulbarichy <ashikul.chowdhury@northsouth.edu>
2025-07-11 20:21:43 +0600

    0.9.3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ashikulbarichy.github.io/', // Add this line - match your repo name
+  base: '/', // Add this line - match your repo name
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
